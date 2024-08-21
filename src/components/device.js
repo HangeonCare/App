ae6474496823519acf6ca8a202e52548e5b08a12
bae080311 <s24024@gsm.hs.kr>
2024-08-21 15:51:44 +0900

    feat: connection

--- src/components/device.js
+++ src/components/device.js
@@ -3,10 +3,24 @@ import { View, Text, Image } from "react-native";
 
 export default function Device() {
   return (
-    <View>
-      <Text>할머니</Text>
-      <Text>현재 작동중</Text>
+    <View style={styles.container}>
+      <Image source={require("../assets/connection/cantConnect.png")} />
+      <View>
+        <Text>할머니</Text>
+        <Text>현재 작동중</Text>
+      </View>
       <Image source={require("../assets/menu.png")} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    flex: 1,
+    backgroudnColor: "white",
+  },
+  textWrapper: {
+    justifyContent: "center",
+  },
+});
