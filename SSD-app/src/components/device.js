108b62fd5e449b36f1dcbff7558a782435b2b941
bae080311 <s24024@gsm.hs.kr>
2024-08-24 01:27:12 +0900

    fix: connection design

--- SSD-app/src/components/device.js
+++ SSD-app/src/components/device.js
@@ -6,7 +6,11 @@ export default function Device() {
   return (
     <View>
       <View style={styles.postContainer}>
-        <Image source={require("../assets/connect.png")} resizeMode="contain" />
+        <Image
+          source={require("../assets/connect.png")}
+          resizeMode="contain"
+          style={{ width: 34, height: 34 }}
+        />
         <View>
           <Text>정연이</Text>
           <Text>현재 작동중</Text>
