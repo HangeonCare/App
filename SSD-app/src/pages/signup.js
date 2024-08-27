22b66441cff9b3c7a06d08dd663ba61ddb1a360f
bae080311 <s24024@gsm.hs.kr>
2024-08-27 23:08:14 +0900

    fix: placeholder

--- SSD-app/src/pages/signup.js
+++ SSD-app/src/pages/signup.js
@@ -1,5 +1,36 @@
-import { View } from "react-native";
+import { View, Text, Image, TouchableOpacity } from "react-native";
+import { TextInput } from "react-native-gesture-handler";
+import React from "react";
+import styles from "../styles/styles";
 
 export default function SignUp() {
-  return <View></View>;
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        gap: 30,
+      }}
+    >
+      <Image
+        resizeMode="contain"
+        style={{ width: 300, height: 75 }}
+        source={require("../assets/loginLogo.png")}
+      />
+      <Text style={styles.LoginText}>Create Account</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="아이디나 이메일을 입력하세요..."
+      ></TextInput>
+      <TextInput
+        style={styles.input}
+        placeholder="비밀번호를 입력하세요..."
+      ></TextInput>
+      <TouchableOpacity>
+        <Text style={styles.Button}>로그인</Text>
+      </TouchableOpacity>
+      <Text>계정 만들기</Text>
+    </View>
+  );
 }
