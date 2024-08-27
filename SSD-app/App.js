0c8323a1917513d1b5ab564a632e80acfd51fb1c
bae080311 <s24024@gsm.hs.kr>
2024-08-27 19:53:52 +0900

    fix: router

--- SSD-app/App.js
+++ SSD-app/App.js
@@ -8,6 +8,7 @@ import SideBar from "./src/components/sideBar";
 import Main from "./src/pages/main";
 import LogOut from "./src/components/logOut";
 import Profile from "./src/pages/Profile";
+import Login from "./src/pages/login";
 
 const Stack = createStackNavigator();
 const Drawer = createDrawerNavigator();
@@ -19,7 +20,7 @@ function DrawerNavigator() {
       screenOptions={{ headerShown: false }}
     >
       <Drawer.Screen name="main" component={Main} />
-      <Drawer.Screen name="Settings" component={LogOut} />
+      <Drawer.Screen name="login" component={Login} />
     </Drawer.Navigator>
   );
 }
