9a39c218fb0030989eb7ec70d51639d729cb9241
bae080311 <s24024@gsm.hs.kr>
2024-08-27 20:40:58 +0900

    feat: Login logo fix: login design

--- SSD-app/src/pages/login.js
+++ SSD-app/src/pages/login.js
@@ -1,5 +1,32 @@
-import { View } from "react-native";
+import { View, Text, Image } from "react-native";
+import { TextInput } from "react-native-gesture-handler";
+import React from "react";
+import styles from "../styles/styles";
 
 export default function Login() {
-  return <View></View>;
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        gap: 30,
+      }}
+    >
+      <Image
+        resizeMode="contain"
+        style={{ width: 300, height: 75 }}
+        source={require("../assets/loginLogo.png")}
+      />
+      <Text style={styles.LoginText}>LOGIN</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="아이디를 입력하세요..."
+      ></TextInput>
+      <TextInput
+        style={styles.input}
+        placeholder="비밀번호를 입력하세요..."
+      ></TextInput>
+    </View>
+  );
 }
