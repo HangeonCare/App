543816faa892bf489d0d140eab74bbca7196666a
bae080311 <s24024@gsm.hs.kr>
2024-09-03 23:40:30 +0900

    feat: usestate

--- SSD-app/src/pages/signup.js
+++ SSD-app/src/pages/signup.js
@@ -4,6 +4,22 @@ import React from "react";
 import styles from "../styles/styles";
 
 export default function SignUp() {
+  const [number, setNumber] = React.useState("");
+  const [numberCheck, setNumberCheck] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [passwordCheck, setPasswordCheck] = React.useState("");
+
+  const [isNumber, setIsNumber] = React.useState(false);
+  const [isNumberCheck, setIsNumberCheck] = React.useState(false);
+  const [isPassword, setIsPassword] = React.useState(false);
+  const [isPasswordCheck, setIsPasswordCheck] = React.useState(false);
+
+  const [isNumberError, setIsNumberError] = React.useState(false);
+  const [isNumberCheckError, setIsNumberCheckError] = React.useState(false);
+  const [isPasswordError, setIsPasswordError] = React.useState(false);
+  const [isPasswordCheckError, setIsPasswordCheckError] = React.useState(false);
+  const NumberRegex = /^[0-9]{11}$/;
+  const Passwordregex = /^[a-zA-Z0-9]{8,16}$/;
   return (
     <View
       style={{
@@ -21,16 +37,23 @@ export default function SignUp() {
       <Text style={styles.LoginText}>Create Account</Text>
       <TextInput
         style={styles.input}
-        placeholder="아이디나 이메일을 입력하세요..."
+        placeholder="전화번호를 입력하세요..."
       ></TextInput>
       <TextInput
         style={styles.input}
-        placeholder="비밀번호를 입력하세요..."
+        placeholder="인증번호 6자리를 입력하세요..."
+      ></TextInput>
+      <TextInput
+        style={styles.input}
+        placeholder="사용하실 비밀번호를 입력하세요..."
+      ></TextInput>
+      <TextInput
+        style={styles.input}
+        placeholder="사용하실 비밀번호를 재입력하세요..."
       ></TextInput>
       <TouchableOpacity>
-        <Text style={styles.Button}>로그인</Text>
+        <Text style={styles.Button}>Create Your Account!</Text>
       </TouchableOpacity>
-      <Text>계정 만들기</Text>
     </View>
   );
 }
