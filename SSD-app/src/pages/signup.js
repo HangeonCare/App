fdb450040280235c7eff4b20e1a5f84e470e47b5
bae080311 <s24024@gsm.hs.kr>
2024-09-09 23:18:32 +0900

    fix: event, fix usestate error

--- SSD-app/src/pages/signup.js
+++ SSD-app/src/pages/signup.js
@@ -1,49 +1,41 @@
-import { View, Text, Image, TouchableOpacity } from "react-native";
-import { TextInput } from "react-native-gesture-handler";
-import React from "react";
-import styles from "../styles/styles";
-import axios from "axios";
-
 export default function SignUp() {
   const [number, setNumber] = React.useState("");
-  const [numberCheck, setNumberCheck] = React.useState("");
   const [password, setPassword] = React.useState("");
   const [passwordCheck, setPasswordCheck] = React.useState("");
 
-  const [isNumber, setIsNumber] = React.useState(false);
-  const [isNumberCheck, setIsNumberCheck] = React.useState(false);
-  const [isPassword, setIsPassword] = React.useState(false);
-  const [isPasswordCheck, setIsPasswordCheck] = React.useState(false);
+  const [numberError, setNumberError] = React.useState("");
+  const [passwordError, setPasswordError] = React.useState("");
+  const [passwordCheckError, setPasswordCheckError] = React.useState("");
 
-  const [NumberError, setNumberError] = React.useState("");
-  const [NumberCheckError, setNumberCheckError] = React.useState("");
-  const [PasswordError, setPasswordError] = React.useState("");
-  const [PasswordCheckError, setPasswordCheckError] = React.useState("");
   const NumberRegex = /^[0-9]{11}$/;
-  const Passwordregex = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,16}$/;
-  function numberCheck() {
-    if (NumberRegex.test(number)) {
-      setNumber(number);
-    } else {
+  const PasswordRegex = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,16}$/;
+
+  function validatePhoneNumber() {
+    if (!NumberRegex.test(number)) {
       setNumberError("전화번호를 다시 확인해주세요");
+    } else {
+      setNumberError("");
     }
   }
-  function checkPasswordcheck() {
-    if (passwordCheck === password) {
-      setPasswordCheck(passwordCheck);
-    } else {
-      setPasswordCheckError("비밀번호가 일치하지 않습니다.");
-    }
-  }
-  function passwordCheck() {
-    if (Passwordregex.test(password)) {
-      setPassword(password);
-    } else {
+
+  function validatePassword() {
+    if (!PasswordRegex.test(password)) {
       setPasswordError(
         "비밀번호는 8자 이상 16자 이내, 특수문자가 들어가야 합니다!"
       );
+    } else {
+      setPasswordError("");
     }
   }
+
+  function checkPasswordMatch() {
+    if (password !== passwordCheck) {
+      setPasswordCheckError("비밀번호가 일치하지 않습니다.");
+    } else {
+      setPasswordCheckError("");
+    }
+  }
+
   function send() {
     axios
       .post("url", {
@@ -51,12 +43,13 @@ export default function SignUp() {
         password: password,
       })
       .then((res) => {
-        localStorage.setItem(res.data);
+        localStorage.setItem("userData", res.data);
       })
       .catch((err) => {
         console.log(err);
       });
   }
+
   return (
     <View
       style={{
@@ -72,34 +65,41 @@ export default function SignUp() {
         source={require("../assets/loginLogo.png")}
       />
       <Text style={styles.LoginText}>Create Account</Text>
+
       <TextInput
-        onChange={numberCheck}
-        required
+        onChangeText={(text) => setNumber(text)}
+        onEndEditing={validatePhoneNumber}
         style={styles.input}
         placeholder="전화번호를 입력하세요..."
-      ></TextInput>
-      <Text>{NumberError}</Text>
+      />
+      <Text>{numberError}</Text>
+
       <TextInput
         maxLength={6}
         keyboardType="numeric"
-        required
         style={styles.input}
         placeholder="인증번호 6자리를 입력하세요..."
-      ></TextInput>
-      <Text>{NumberCheckError}</Text>
+      />
+      <Text>{numberError}</Text>
+
       <TextInput
-        onChange={passwordCheck}
-        required
+        secureTextEntry={true}
+        onChangeText={(text) => setPassword(text)}
+        onEndEditing={validatePassword}
         style={styles.input}
         placeholder="사용하실 비밀번호를 입력하세요..."
-      ></TextInput>
-      <Text>{PasswordError}</Text>
+      />
+      <Text>{passwordError}</Text>
+
       <TextInput
-        required
+        secureTextEntry={true}
+        onChangeText={(text) => setPasswordCheck(text)}
+        onEndEditing={checkPasswordMatch}
         style={styles.input}
         placeholder="사용하실 비밀번호를 재입력하세요..."
-      ></TextInput>
-      <Text>{PasswordCheckError}</Text>
+      />
+      <Text>{passwordCheckError}</Text>
+
       <TouchableOpacity onPress={send}>
         <Text style={styles.Button}>Create Your Account!</Text>
       </TouchableOpacity>
