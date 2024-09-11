55f4039f601a8e79de328e151654c86e2ab5d94d
bae080311 <s24024@gsm.hs.kr>
2024-09-11 18:18:14 +0900

    feat: 경로

--- SSD-app/src/pages/main.js
+++ SSD-app/src/pages/main.js
@@ -8,7 +8,7 @@ import styles from "../styles/styles";
 export default function Main({ navigation }) {
   return (
     <View style={styles.container}>
-      <Header navigation={navigation} />
+      <Header navigation={navigation.navigate("Drawer")} />
       <Text style={styles.title}>기기</Text>
       <Device />
       <AddDevice />
