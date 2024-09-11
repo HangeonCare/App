5b8637826533b17ac281783ed802c516224e00f8
bae080311 <s24024@gsm.hs.kr>
2024-09-11 15:48:03 +0900

    fix: touchableOpacity

--- SSD-app/src/components/popUp.js
+++ SSD-app/src/components/popUp.js
@@ -1,24 +1,9 @@
-import { View, Image, Text } from "react-native";
+import { View, Image, Text, TouchableOpacity } from "react-native";
 import styles from "../styles/styles";
 
 export default function PopUp() {
   return (
     <View style={styles.popup}>
-      <View style={styles.option}>
-        <Image
-          style={styles.icon}
-          resizeMode="contain"
-          source={require("../assets/information.png")}
-        ></Image>
-        <Text
-          style={{
-            fontSize: 16,
-            fontWeight: "700",
-          }}
-        >
-          정보
-        </Text>
-      </View>
       <View style={styles.option}>
         <Text
           style={{
@@ -29,38 +14,59 @@ export default function PopUp() {
           기기 이름
         </Text>
       </View>
-      <View style={styles.option}>
-        <Image
-          resizeMode="contain"
-          style={styles.icon}
-          source={require("../assets/date.png")}
-        ></Image>
-        <Text
-          style={{
-            fontSize: 16,
-            fontWeight: "700",
-            color: "#180161",
-          }}
-        >
-          날짜 수정
-        </Text>
-      </View>
-      <View style={styles.option}>
-        <Image
-          resizeMode="contain"
-          style={styles.icon}
-          source={require("../assets/delete.png")}
-        />
-        <Text
-          style={{
-            fontSize: 16,
-            fontWeight: "700",
-            color: "#ff0000",
-          }}
-        >
-          삭제
-        </Text>
-      </View>
+      <TouchableOpacity>
+        <View style={styles.option}>
+          <Image
+            style={styles.icon}
+            resizeMode="contain"
+            source={require("../assets/information.png")}
+          ></Image>
+          <Text
+            style={{
+              fontSize: 16,
+              fontWeight: "700",
+            }}
+          >
+            정보
+          </Text>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <View style={styles.option}>
+          <Image
+            resizeMode="contain"
+            style={styles.icon}
+            source={require("../assets/date.png")}
+          ></Image>
+          <Text
+            style={{
+              fontSize: 16,
+              fontWeight: "700",
+              color: "#180161",
+            }}
+          >
+            날짜 수정
+          </Text>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <View style={styles.option}>
+          <Image
+            resizeMode="contain"
+            style={styles.icon}
+            source={require("../assets/delete.png")}
+          />
+          <Text
+            style={{
+              fontSize: 16,
+              fontWeight: "700",
+              color: "#ff0000",
+            }}
+          >
+            삭제
+          </Text>
+        </View>
+      </TouchableOpacity>
     </View>
   );
 }
