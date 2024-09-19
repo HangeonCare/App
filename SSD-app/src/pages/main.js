ebfa002f9d573c11217d7fac78f8391a106b6033
bae080311 <s24024@gsm.hs.kr>
2024-09-19 21:06:56 +0900

    feat: get data

--- SSD-app/src/pages/main.js
+++ SSD-app/src/pages/main.js
@@ -4,14 +4,23 @@ import Device from "../components/device";
 import AddDevice from "../components/addDevice";
 import Header from "../components/header";
 import styles from "../styles/styles";
+import DurationPicker from "../components/setDate";
 
 export default function Main({ navigation }) {
+  function getDeviceData(){
+    axios.get("url").tten(res=>{
+      localStorage.setItem(res.data);
+    }).catch(err=>{
+      console.log(err);
+    })
+  }
   return (
     <View style={styles.container}>
       <Header navigation={navigation.navigate("Drawer")} />
       <Text style={styles.title}>기기</Text>
       <Device />
       <AddDevice />
+      <DurationPicker />
     </View>
   );
 }
