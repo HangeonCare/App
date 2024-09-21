5e9612d13bb5447b0b1f641b6ab2e88ea782cfbe
bae080311 <s24024@gsm.hs.kr>
2024-09-22 01:22:56 +0900

    fix: keyboard setting

--- SSD-app/src/pages/registerDevice.js
+++ SSD-app/src/pages/registerDevice.js
@@ -1,4 +1,12 @@
-import { Image, View, Text, TextInput, TouchableOpacity } from "react-native";
+import {
+  Image,
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
 import styles from "../styles/styles";
 import axios from "axios";
 import React, { useState } from "react";
@@ -26,35 +34,39 @@ export default function RegisterDevice({ navigation }) {
   }
 
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        gap: 60,
-      }}
-    >
-      <Image
-        resizeMode="contain"
-        style={{ width: 100, height: 40 }}
-        source={require("../assets/SSDlogo.png")}
-      />
-      <Text style={styles.LoginText}>기기 연결</Text>
-      <TextInput
-        value={number}
-        onChangeText={(text) => setNumber(text)}
-        placeholder="시리얼 번호를 입력하세요..."
-        style={styles.input}
-      />
-      {numberError ? <Text style={{ color: "red" }}>{numberError}</Text> : null}
-      <View style={{ display: "flex", gap: 12 }}>
-        <TouchableOpacity onPress={send}>
-          <Text style={styles.Button}>연결하기</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => navigation.goBack()}>
-          <Text style={styles.Button}>돌아가기</Text>
-        </TouchableOpacity>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+          gap: 60,
+        }}
+      >
+        <Image
+          resizeMode="contain"
+          style={{ width: 100, height: 40 }}
+          source={require("../assets/SSDlogo.png")}
+        />
+        <Text style={styles.LoginText}>기기 연결</Text>
+        <TextInput
+          value={number}
+          onChangeText={(text) => setNumber(text)}
+          placeholder="시리얼 번호를 입력하세요..."
+          style={styles.input}
+        />
+        {numberError ? (
+          <Text style={{ color: "red" }}>{numberError}</Text>
+        ) : null}
+        <View style={{ display: "flex", gap: 12 }}>
+          <TouchableOpacity onPress={send}>
+            <Text style={styles.Button}>연결하기</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => navigation.goBack()}>
+            <Text style={styles.Button}>돌아가기</Text>
+          </TouchableOpacity>
+        </View>
       </View>
-    </View>
+    </TouchableWithoutFeedback>
   );
 }
