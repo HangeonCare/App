69264ca91ad3d58a5298536d0849325eeb1ab34c
bae080311 <s24024@gsm.hs.kr>
2024-09-22 02:17:13 +0900

    feat: error message fix: keyboard setting

--- SSD-app/src/pages/signup.js
+++ SSD-app/src/pages/signup.js
@@ -1,11 +1,27 @@
-export default function SignUp({ navigation }) {
-  const [number, setNumber] = React.useState("");
-  const [password, setPassword] = React.useState("");
-  const [passwordCheck, setPasswordCheck] = React.useState("");
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  TextInput,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
+import axios from "axios";
 
-  const [numberError, setNumberError] = React.useState("");
-  const [passwordError, setPasswordError] = React.useState("");
-  const [passwordCheckError, setPasswordCheckError] = React.useState("");
+export default function SignUp({ navigation }) {
+  const [number, setNumber] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordCheck, setPasswordCheck] = useState("");
+
+  const [numberError, setNumberError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const [passwordCheckError, setPasswordCheckError] = useState("");
+
+  const [numberTouched, setNumberTouched] = useState(false);
+  const [passwordTouched, setPasswordTouched] = useState(false);
+  const [passwordCheckTouched, setPasswordCheckTouched] = useState(false);
 
   const NumberRegex = /^[0-9]{11}$/;
   const PasswordRegex = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,16}$/;
@@ -45,65 +61,81 @@ export default function SignUp({ navigation }) {
       .then((res) => {
         navigation.navigate("Login");
         console.log(res);
+        alert("회원가입이 완료되었습니다.");
       })
       .catch((err) => {
         console.log(err);
+        alert("조건에 맞게 입력해주세요");
       });
   }
 
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        gap: 30,
-      }}
-    >
-      <Image
-        resizeMode="contain"
-        style={{ width: 300, height: 75 }}
-        source={require("../assets/loginLogo.png")}
-      />
-      <Text style={styles.LoginText}>Create Account</Text>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+          gap: 20,
+        }}
+      >
+        <Image
+          resizeMode="contain"
+          style={{ width: 300, height: 75 }}
+          source={require("../assets/loginLogo.png")}
+        />
+        <Text style={styles.LoginText}>Create Account</Text>
 
-      <TextInput
-        onChangeText={(text) => setNumber(text)}
-        onEndEditing={validatePhoneNumber}
-        style={styles.input}
-        placeholder="전화번호를 입력하세요..."
-      />
-      <Text>{numberError}</Text>
+        <TextInput
+          onChangeText={(text) => setNumber(text)}
+          onBlur={() => {
+            setNumberTouched(true);
+            validatePhoneNumber();
+          }}
+          style={styles.input}
+          placeholder="전화번호를 입력하세요..."
+        />
+        {numberTouched && <Text style={{ color: "red" }}>{numberError}</Text>}
 
-      <TextInput
-        maxLength={6}
-        keyboardType="numeric"
-        style={styles.input}
-        placeholder="인증번호 6자리를 입력하세요..."
-      />
-      <Text>{numberError}</Text>
+        <TextInput
+          maxLength={6}
+          keyboardType="numeric"
+          style={styles.input}
+          placeholder="인증번호 6자리를 입력하세요..."
+        />
 
-      <TextInput
-        secureTextEntry={true}
-        onChangeText={(text) => setPassword(text)}
-        onEndEditing={validatePassword}
-        style={styles.input}
-        placeholder="사용하실 비밀번호를 입력하세요..."
-      />
-      <Text>{passwordError}</Text>
+        <TextInput
+          secureTextEntry={true}
+          onChangeText={(text) => setPassword(text)}
+          onBlur={() => {
+            setPasswordTouched(true);
+            validatePassword();
+          }}
+          style={styles.input}
+          placeholder="사용하실 비밀번호를 입력하세요..."
+        />
+        {passwordTouched && (
+          <Text style={{ color: "red" }}>{passwordError}</Text>
+        )}
 
-      <TextInput
-        secureTextEntry={true}
-        onChangeText={(text) => setPasswordCheck(text)}
-        onEndEditing={checkPasswordMatch}
-        style={styles.input}
-        placeholder="사용하실 비밀번호를 재입력하세요..."
-      />
-      <Text>{passwordCheckError}</Text>
+        <TextInput
+          secureTextEntry={true}
+          onChangeText={(text) => setPasswordCheck(text)}
+          onBlur={() => {
+            setPasswordCheckTouched(true);
+            checkPasswordMatch();
+          }}
+          style={styles.input}
+          placeholder="사용하실 비밀번호를 재입력하세요..."
+        />
+        {passwordCheckTouched && (
+          <Text style={{ color: "red" }}>{passwordCheckError}</Text>
+        )}
 
-      <TouchableOpacity onPress={send}>
-        <Text style={styles.Button}>Create Your Account!</Text>
-      </TouchableOpacity>
-    </View>
+        <TouchableOpacity onPress={send}>
+          <Text style={styles.Button}>Create Your Account!</Text>
+        </TouchableOpacity>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
