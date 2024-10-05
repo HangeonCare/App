45a2bc5270dcf4421209a75465f12db2a7365ffb
bae080311 <s24024@gsm.hs.kr>
2024-10-05 21:00:13 +0900

    fix: popup delete confirm

--- SSD-app/src/components/popUp.js
+++ SSD-app/src/components/popUp.js
@@ -1,5 +1,12 @@
 import React, { useState } from "react";
-import { View, Image, Text, TouchableOpacity, Modal } from "react-native";
+import {
+  View,
+  Image,
+  Text,
+  TouchableOpacity,
+  Modal,
+  Alert,
+} from "react-native";
 import styles from "../styles/styles";
 import DurationPicker from "./setDate";
 import axios from "axios";
@@ -12,21 +19,35 @@ export default function PopUp({ serialNumber, onClose }) {
   const [isDatePickerVisible, setDatePickerVisible] = useState(false);
 
   function DeleteData() {
-    let confirmDelete = confirm("정말 삭제하시겠습니까?");
-    if (confirmDelete) {
-      axios
-        .delete(`${url}/users/${id}/devices/${serialNumber}`, {
-          serialNumber: serialNumber,
-        })
-        .then((res) => {
-          alert("삭제되었습니다.");
-          onClose();
-        })
-        .catch((err) => {
-          console.log(err);
-          alert("삭제에 실패했습니다.");
-        });
-    }
+    Alert.alert(
+      "삭제 확인",
+      "정말 삭제하시겠습니까?",
+      [
+        {
+          text: "취소",
+          style: "cancel",
+        },
+        {
+          text: "삭제",
+          onPress: () => {
+            axios
+              .delete(`${url}/users/${id}/devices/${serialNumber}`, {
+                serialNumber: serialNumber,
+              })
+              .then((res) => {
+                alert("삭제되었습니다.");
+                onClose();
+              })
+              .catch((err) => {
+                console.log(err);
+                alert("삭제에 실패했습니다.");
+              });
+          },
+          style: "destructive",
+        },
+      ],
+      { cancelable: true }
+    );
   }
 
   return (
