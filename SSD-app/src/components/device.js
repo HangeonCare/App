2f9dfc5b46671bdb3cc0c0bf4d6e74c142a55be8
bae080311 <s24024@gsm.hs.kr>
2024-10-05 20:50:45 +0900

    fix: device container

--- SSD-app/src/components/device.js
+++ SSD-app/src/components/device.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   View,
   Text,
@@ -10,16 +10,15 @@ import {
 } from "react-native";
 import styles from "../styles/styles";
 import PopUp from "./popUp";
-import DurationPicker from "./setDate";
 
 export default function Device({ devicedata }) {
   const [toggle, setToggle] = useState(false);
   const [device, setDevice] = useState({});
+
   useEffect(() => {
     setDevice(devicedata);
-    PopUp(device.serialNumber);
-    DurationPicker(device.serialNumber);
-  }, []);
+  }, [devicedata]);
+
   const handleToggle = () => {
     setToggle(!toggle);
   };
@@ -29,41 +28,39 @@ export default function Device({ devicedata }) {
   };
 
   return (
-    <View key={device.serialNumber}>
-      <View style={styles.postContainer}>
-        <Image
-          source={require("../assets/connect.png")}
-          resizeMode="contain"
-          style={{ width: 34, height: 34 }}
-        />
-        <View style={{ marginLeft: -140 }}>
-          <Text style={styles.user}>{device.serialNumber}</Text>
-          <Text style={styles.time}>{device.period}</Text>
-        </View>
-        <TouchableOpacity onPress={handleToggle}>
-          <Image
-            source={require("../assets/menu.png")}
-            resizeMode="contain"
-            style={styles.icon}
-          />
-        </TouchableOpacity>
-        <Modal
-          transparent={true}
-          visible={toggle}
-          animationType="none"
-          onRequestClose={handleCloseModal}
-        >
-          <TouchableWithoutFeedback onPress={handleCloseModal}>
-            <View style={modalStyles.modalBackground}>
-              <TouchableWithoutFeedback>
-                <View style={modalStyles.modalContainer}>
-                  <PopUp />
-                </View>
-              </TouchableWithoutFeedback>
-            </View>
-          </TouchableWithoutFeedback>
-        </Modal>
+    <View key={devicedata.serialNumber} style={styles.postContainer}>
+      <Image
+        source={require("../assets/connect.png")}
+        resizeMode="contain"
+        style={{ width: 34, height: 34 }}
+      />
+      <View style={{ marginLeft: -140 }}>
+        <Text style={styles.user}>{devicedata.serialNumber}</Text>
+        <Text style={styles.time}>{devicedata.period}</Text>
       </View>
+      <TouchableOpacity onPress={handleToggle}>
+        <Image
+          source={require("../assets/menu.png")}
+          resizeMode="contain"
+          style={styles.icon}
+        />
+      </TouchableOpacity>
+      <Modal
+        transparent={true}
+        visible={toggle}
+        animationType="none"
+        onRequestClose={handleCloseModal}
+      >
+        <TouchableWithoutFeedback onPress={handleCloseModal}>
+          <View style={modalStyles.modalBackground}>
+            <TouchableWithoutFeedback>
+              <View style={modalStyles.modalContainer}>
+                <PopUp />
+              </View>
+            </TouchableWithoutFeedback>
+          </View>
+        </TouchableWithoutFeedback>
+      </Modal>
     </View>
   );
 }
