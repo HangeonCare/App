832db3964bcd57924b57e2858828954e2c2771cc
bae080311 <s24024@gsm.hs.kr>
2024-10-05 20:52:27 +0900

    feat: 비동기로 id 값 받기

--- SSD-app/src/pages/registerDevice.js
+++ SSD-app/src/pages/registerDevice.js
@@ -6,34 +6,51 @@ import {
   TouchableOpacity,
   TouchableWithoutFeedback,
   Keyboard,
+  Alert,
 } from "react-native";
 import styles from "../styles/styles";
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const url = "https://port-0-bes-m1ed5avw1d3364c3.sel4.cloudtype.app";
-const id = AsyncStorage.getItem("id");
 
 export default function RegisterDevice({ navigation }) {
   const [number, setNumber] = useState("");
   const [numberError, setNumberError] = useState("");
+  const [id, setId] = useState(null);
+  useEffect(() => {
+    const getID = async () => {
+      try {
+        const Id = await AsyncStorage.getItem("id");
+        if (Id !== null) {
+          setId(Id);
+          await send();
+        } else {
+          setNumberError("ID가 존재하지 않습니다.");
+        }
+      } catch (error) {
+        console.log(error);
+        Alert.alert("ID를 불러오는 데 실패했습니다.");
+      }
+    };
+    getID();
+  }, []);
 
-  function send() {
+  async function send() {
     if (number === "") {
       setNumberError("시리얼 번호를 입력하세요.");
       return;
-    } else {
-      axios
-        .post(`${url}/users/${id}/devices`, { serial_number: number })
-        .then((res) => {
-          console.log(res);
-          navigation.navigate("Drawer");
-        })
-        .catch((err) => {
-          console.log(err);
-          setNumberError("시리얼 번호를 다시 확인해주세요");
-        });
+    }
+    try {
+      const res = await axios.post(`${url}/users/${id}/devices`, {
+        serial_number: number,
+      });
+      console.log(res);
+      navigation.navigate("Drawer");
+    } catch (err) {
+      console.log(err);
+      setNumberError("시리얼 번호를 다시 확인해주세요");
     }
   }
 
