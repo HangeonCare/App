cd0d9641687aeeb3f052ec175256dfcfd4863c6a
bae080311 <s24024@gsm.hs.kr>
2024-10-31 21:00:48 +0900

    Remove: 🔥 hamberger button Feat: ✨ 로그아웃

--- SSD-app/src/components/header.js
+++ SSD-app/src/components/header.js
@@ -1,25 +1,33 @@
 import React from "react";
-import { Image, View, TouchableOpacity } from "react-native";
-import { DrawerActions } from "@react-navigation/native";
+import { Image, View, TouchableOpacity, Text } from "react-native";
 import styles from "../styles/styles";
+import axios from "axios";
+
+const url = "https://port-0-bes-m1ed5avw1d3364c3.sel4.cloudtype.app";
 
 export default function Header({ navigation }) {
+  const logout = () => {
+    axios
+      .post(`${url}/users/logout`)
+      .then((res) => {
+        alert("로그아웃 되었습니다");
+        navigation.navigate("login");
+      })
+      .catch((err) => {
+        console.log(err);
+        alert("로그아웃 실패");
+      });
+  };
   return (
     <View style={styles.header}>
-      <TouchableOpacity
-        onPress={() => navigation.dispatch(DrawerActions.toggleDrawer())}
-      >
-        <Image
-          style={styles.icon}
-          resizeMode="contain"
-          source={require("../assets/hamberger.png")}
-        />
-      </TouchableOpacity>
       <Image
         style={styles.logo}
         resizeMode="contain"
         source={require("../assets/SSDlogo.png")}
       />
+      <TouchableOpacity onPress={logout}>
+        <Text>로그아웃</Text>
+      </TouchableOpacity>
     </View>
   );
 }
