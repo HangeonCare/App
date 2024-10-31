0fbba85d7bc1f173f31827151dbe3b2bf71cf008
bae080311 <s24024@gsm.hs.kr>
2024-10-31 20:11:48 +0900

    fix: login function

--- SSD-app/src/pages/login.js
+++ SSD-app/src/pages/login.js
@@ -1,160 +1,140 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import axios from "axios";
 import React, { useState } from "react";
 import {
   View,
   Text,
-  Image,
+  TextInput,
   TouchableOpacity,
+  Image,
   TouchableWithoutFeedback,
   Keyboard,
 } from "react-native";
-import { TextInput } from "react-native-gesture-handler";
-import styles from "../styles/styles";
-import axios from "axios";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import SSDlogo from "../assets/SSDlogo.png";
-import Main from "./main";
 
-const url = "https://port-0-bes-m1ed5avw1d3364c3.sel4.cloudtype.app";
-
-export default function Login({ navigation }) {
+export function Login({ navigation }) {
   const [number, setNumber] = useState("");
   const [password, setPassword] = useState("");
-
-  const [numberError, setNumberError] = useState("");
-  const [passwordError, setPasswordError] = useState("");
-
-  const [numberTouched, setNumberTouched] = useState(false);
-  const [passwordTouched, setPasswordTouched] = useState(false);
-
-  function validatePhoneNumber() {
-    if (number === "") {
-      setNumberError("전화번호를 입력하세요.");
-    } else {
-      setNumberError("");
-    }
-  }
-
-  function validatePassword() {
-    if (password === "") {
-      setPasswordError("비밀번호를 입력하세요.");
-    } else {
-      setPasswordError("");
-    }
-  }
+  const [error, setError] = useState(false);
 
   function send() {
-    validatePhoneNumber();
-    validatePassword();
-
-    if (!numberError && !passwordError) {
-      axios
-        .post(`${url}/users/login`, {
+    axios
+      .post(
+        "https://port-0-bes-m1ed5avw1d3364c3.sel4.cloudtype.app/users/login",
+        {
           phoneNumber: number,
           password: password,
-        })
-        .then((res) => {
-          AsyncStorage.setItem("id", JSON.stringify(res.data.userId));
-          Main(number);
-          navigation.navigate("Drawer");
-        })
-        .catch((err) => {
-          console.log(err);
-          setNumberError("전화번호를 다시 확인해주세요");
-          setPasswordError("비밀번호를 다시 확인해주세요");
-          setNumber("");
-          setPassword("");
-        });
-    }
+        }
+      )
+      .then((res) => {
+        console.log(res);
+        AsyncStorage.setItem("id", res.data.id);
+        navigation.navigate("Main");
+      })
+      .catch((err) => {
+        console.log(err);
+        setError(true);
+        setNumber("");
+        setPassword("");
+      });
   }
 
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
       <View
         style={{
-          padding: 20,
-          paddingTop: 100,
-          paddingBottom: 40,
           flex: 1,
-          justifyContent: "space-between",
-          alignItems: "start",
-          gap: 20,
+          justifyContent: "center",
+          padding: 20,
         }}
       >
-        <View>
-          <View style={{ marginBottom: 30 }}>
-            <Image
-              resizeMode="contain"
-              style={{ width: 100, height: 40, marginBottom: 15 }}
-              source={SSDlogo}
+        <View style={{ marginTop: 130, flex: 0.8, gap: 15 }}>
+          <Image
+            style={{ width: 100, height: 40 }}
+            source={require("../assets/SSDlogo.png")}
+          />
+          <Text style={{ color: "#555555" }}>
+            로그인 후 기기를 등록 할 수 있어요
+          </Text>
+        </View>
+        <View style={{ flex: 2 }}>
+          <Text style={{ fontWeight: 600, fontSize: 28, marginBottom: 40 }}>
+            로그인
+          </Text>
+          <View>
+            <TextInput
+              keyboardType="numeric"
+              placeholder="전화번호를 숫자만 입력해주세요"
+              style={{
+                width: "100%",
+                height: 50,
+                borderColor: "gray",
+                borderBottomWidth: 1,
+                marginBottom: 20,
+                paddingHorizontal: 10,
+              }}
+              onChangeText={setNumber}
+              value={number}
             />
-            <Text style={{ color: "#555555" }}>
-              로그인 후 기기를 등록 할 수 있어요
-            </Text>
+            {error && <Text>전화번호를 입력해주세요</Text>}
           </View>
-          <Text style={styles.LoginText}>로그인</Text>
-          <View style={{ display: "flex", gap: 20 }}>
-            <View>
-              <TextInput
-                style={styles.input}
-                placeholder="전화번호를 숫자만 입력해주세요"
-                value={number}
-                onChangeText={(text) => {
-                  setNumber(text);
-                  if (numberTouched) validatePhoneNumber();
-                }}
-                onBlur={() => {
-                  setNumberTouched(true);
-                  validatePhoneNumber();
-                }}
-              />
-              {numberTouched && (
-                <Text style={{ color: "red" }}>{numberError}</Text>
-              )}
-            </View>
-            <View>
-              <TextInput
-                style={styles.input}
-                placeholder="비밀번호를 입력하세요..."
-                secureTextEntry
-                value={password}
-                onChangeText={(text) => {
-                  setPassword(text);
-                  if (passwordTouched) validatePassword();
-                }}
-                onBlur={() => {
-                  setPasswordTouched(true);
-                  validatePassword();
-                }}
-              />
-              {passwordTouched && (
-                <Text style={{ color: "red" }}>{passwordError}</Text>
-              )}
-            </View>
+          <View>
+            <TextInput
+              keyboardType="numeric"
+              secureTextEntry={true}
+              placeholder="비밀번호를 입력하세요"
+              style={{
+                width: "100%",
+                height: 50,
+                borderColor: "gray",
+                borderBottomWidth: 1,
+                marginBottom: 20,
+                paddingHorizontal: 10,
+              }}
+              onChangeText={setPassword}
+              value={password}
+            />
+            {error && <Text>비밀번호를 입력해주세요</Text>}
           </View>
         </View>
-        <View style={{ display: "flex", gap: "20" }}>
+        <View>
           <TouchableOpacity
+            onPress={send}
             style={{
               paddingVertical: 18,
               paddingHorizontal: 140,
               borderRadius: 12,
-              backgroundColor: number && password ? "#EB3678" : "#E1E1E1",
+              backgroundColor:
+                password === "" && number === "" ? "#E1E1E1" : "#EB3678",
             }}
-            onPress={send}
           >
             <Text
               style={{
                 textAlign: "center",
                 fontSize: 16,
                 fontWeight: "600",
-                color: number && password ? "white" : "black",
+                color: password === "" && number === "" ? "black" : "white",
               }}
             >
               로그인
             </Text>
           </TouchableOpacity>
           <TouchableOpacity onPress={() => navigation.navigate("SignUp")}>
-            <Text style={styles.Button}>회원가입</Text>
+            <Text
+              style={{
+                paddingVertical: 18,
+                paddingHorizontal: 140,
+                borderRadius: 12,
+                textAlign: "center",
+                borderWidth: 1,
+                borderColor: "#B7B7B7",
+                fontSize: 16,
+                fontWeight: "600",
+                marginBottom: 70,
+                marginTop: 20,
+              }}
+            >
+              회원가입
+            </Text>
           </TouchableOpacity>
         </View>
       </View>
