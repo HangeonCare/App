8b6ff7bb1e83361aa85fcb322927e572d93d5b06
bae080311 <s24024@gsm.hs.kr>
2024-10-31 20:52:28 +0900

    Feat: alert

--- SSD-app/src/components/device.js
+++ SSD-app/src/components/device.js
@@ -7,12 +7,61 @@ import {
   Modal,
   StyleSheet,
   TouchableWithoutFeedback,
+  Alert,
 } from "react-native";
 import styles from "../styles/styles";
-import PopUp from "./popUp";
+import DurationPicker from "./setDate";
+import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
+const url = "https://port-0-bes-m1ed5avw1d3364c3.sel4.cloudtype.app";
+
+function DeleteData() {
+  Alert.alert(
+    "삭제 확인",
+    "정말 삭제하시겠습니까?",
+    [
+      {
+        text: "취소",
+        style: "cancel",
+      },
+      {
+        text: "삭제",
+        onPress: deleteData,
+        style: "destructive",
+      },
+    ],
+    { cancelable: true }
+  );
+}
+async function deleteData() {
+  try {
+    const res = await axios.delete(`${url}/users/3/devices/ABC123`);
+    alert("삭제되었습니다.");
+    onClose();
+  } catch (err) {
+    console.log(err);
+    alert("삭제에 실패했습니다.");
+  }
+}
 export default function Device({ devicedata }) {
   const [toggle, setToggle] = useState(false);
+  const [isDatePickerVisible, setDatePickerVisible] = useState(false);
+  useEffect(() => {
+    const fetchIdAndDeviceData = async () => {
+      try {
+        const storedId = await AsyncStorage.getItem("id");
+        if (storedId) {
+          await deleteData(storedId);
+        }
+      } catch (error) {
+        console.log(error);
+        Alert.alert("ID를 불러오는 데 실패했습니다.");
+      }
+    };
+
+    fetchIdAndDeviceData();
+  }, []);
 
   const handleToggle = () => {
     setToggle(!toggle);
@@ -50,7 +99,56 @@ export default function Device({ devicedata }) {
           <View style={modalStyles.modalBackground}>
             <TouchableWithoutFeedback>
               <View style={modalStyles.modalContainer}>
-                <PopUp />
+                <View style={styles.popup}>
+                  <View style={styles.option}>
+                    <Text style={{ fontSize: 16, fontWeight: "700" }}>
+                      {devicedata.serialNumber}
+                    </Text>
+                  </View>
+                  <TouchableOpacity onPress={() => setDatePickerVisible(true)}>
+                    <View style={styles.option}>
+                      <Image
+                        resizeMode="contain"
+                        style={styles.icon}
+                        source={require("../assets/date.png")}
+                      />
+                      <Text
+                        style={{
+                          fontSize: 16,
+                          fontWeight: "700",
+                          color: "#180161",
+                        }}
+                      >
+                        날짜 수정
+                      </Text>
+                    </View>
+                  </TouchableOpacity>
+
+                  <TouchableOpacity onPress={DeleteData}>
+                    <View style={styles.option}>
+                      <Image
+                        resizeMode="contain"
+                        style={styles.icon}
+                        source={require("../assets/delete.png")}
+                      />
+                      <Text
+                        style={{
+                          fontSize: 16,
+                          fontWeight: "700",
+                          color: "#ff0000",
+                        }}
+                      >
+                        삭제
+                      </Text>
+                    </View>
+                  </TouchableOpacity>
+
+                  <Modal visible={isDatePickerVisible} animationType="slide">
+                    <DurationPicker
+                      onClose={() => setDatePickerVisible(false)}
+                    />
+                  </Modal>
+                </View>
               </View>
             </TouchableWithoutFeedback>
           </View>
