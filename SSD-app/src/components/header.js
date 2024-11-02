109c662bc27b247733ea9b9c9282a87cda0b2a99
bae080311 <s24024@gsm.hs.kr>
2024-11-02 16:00:52 +0900

    Fix: logout🐛

--- SSD-app/src/components/header.js
+++ SSD-app/src/components/header.js
@@ -1,22 +1,12 @@
 import React from "react";
 import { Image, View, TouchableOpacity, Text } from "react-native";
 import styles from "../styles/styles";
-import axios from "axios";
-
-const url = "https://port-0-bes-m1ed5avw1d3364c3.sel4.cloudtype.app";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function Header({ navigation }) {
   const logout = () => {
-    axios
-      .post(`${url}/users/logout`)
-      .then((res) => {
-        alert("로그아웃 되었습니다");
-        navigation.navigate("login");
-      })
-      .catch((err) => {
-        console.log(err);
-        alert("로그아웃 실패");
-      });
+    AsyncStorage.removeItem("id");
+    navigation.navigate("login");
   };
   return (
     <View style={styles.header}>
