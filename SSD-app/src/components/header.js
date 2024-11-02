bea2f474c8e4908350e49f7056b5e9716f327f68
bae080311 <s24024@gsm.hs.kr>
2024-11-02 16:04:01 +0900

    Feat: ✨confirm log out

--- SSD-app/src/components/header.js
+++ SSD-app/src/components/header.js
@@ -2,8 +2,26 @@ import React from "react";
 import { Image, View, TouchableOpacity, Text } from "react-native";
 import styles from "../styles/styles";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-
+import { Alert } from "react-native";
 export default function Header({ navigation }) {
+  const confirmLogout = () => {
+    Alert.alert(
+      "로그아웃 확인",
+      "정말 로그아웃하시겠습니까?",
+      [
+        {
+          text: "취소",
+          style: "cancel",
+        },
+        {
+          text: "확인",
+          onPress: logout,
+          style: "default",
+        },
+      ],
+      { cancelable: true }
+    );
+  };
   const logout = () => {
     AsyncStorage.removeItem("id");
     navigation.navigate("login");
@@ -15,7 +33,7 @@ export default function Header({ navigation }) {
         resizeMode="contain"
         source={require("../assets/SSDlogo.png")}
       />
-      <TouchableOpacity onPress={logout}>
+      <TouchableOpacity onPress={confirmLogout}>
         <Text>로그아웃</Text>
       </TouchableOpacity>
     </View>
