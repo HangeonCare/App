27eec466cbb28c34ca8fbc5adb18109f1aabe3b7
bae080311 <s24024@gsm.hs.kr>
2024-11-06 12:15:16 +0900

    Feat: ✨ pop up option

--- SSD-app/src/components/device.js
+++ SSD-app/src/components/device.js
@@ -13,10 +13,12 @@ import styles from "../styles/styles";
 import DurationPicker from "./setDate";
 import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useNavigation } from "@react-navigation/native";
 
 const url = "https://port-0-bes-m1ed5avw1d3364c3.sel4.cloudtype.app";
 
 export default function Device({ devicedata }) {
+  const navigation = useNavigation();
   const [toggle, setToggle] = useState(false);
   const [isDatePickerVisible, setDatePickerVisible] = useState(false);
   const [id, setId] = useState(null);
@@ -44,7 +46,6 @@ export default function Device({ devicedata }) {
         `${url}/users/${id}/devices/${devicedata.serialNumber}`
       );
       Alert.alert("삭제되었습니다.");
-      window.location.reload();
       handleCloseModal();
     } catch (err) {
       console.log(err);
@@ -159,6 +160,26 @@ export default function Device({ devicedata }) {
                         }}
                       >
                         삭제
+                      </Text>
+                    </View>
+                  </TouchableOpacity>
+                  <TouchableOpacity
+                    onPress={() => navigation.navigate("Graph")}
+                  >
+                    <View style={styles.option}>
+                      <Image
+                        resizeMode="contain"
+                        style={styles.icon}
+                        source={require("../assets/heartBeat.png")}
+                      />
+                      <Text
+                        style={{
+                          fontSize: 16,
+                          fontWeight: "700",
+                          color: "black",
+                        }}
+                      >
+                        생활패턴 보기
                       </Text>
                     </View>
                   </TouchableOpacity>
